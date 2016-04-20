b678c6dea455bcdf37f5b263ef690d49fe919a7e
scottdomes <scottdomes@gmail.com>
2016-04-20 15:05:38 -0700

    Added location image and input

--- app/containers/VotingContainer.jsx
+++ app/containers/VotingContainer.jsx
@@ -17,6 +17,22 @@ var VotingContainer = React.createClass({
       <div>
         <BackButton prevpage={"/eventconfig"}/>
         <ProgressIndicator page={"vote"}/>
+        <div id="voting-page-heading" className="row">
+          <div className="large-12 large columns text-center">
+            <h3>Osama created the event Bear</h3>
+          </div>
+        </div>
+        <div id="location-options" className="row">
+          <div className="large-12 large columns text-center">
+            <img src="//localhost:3000/img/tent3.jpg" />
+            <button className="location-vote-button button success">Vote for Squamish</button>
+          </div>
+        </div>
+        <div id="add-location" className="row">
+          <div className="large-4 large-centered large columns text-center">
+            <input type="text" placeholder="Add location..." />
+          </div>
+        </div>
         <h3>Start Date: {this.props.dateRange.start}</h3>
         <h3>End Date: {this.props.dateRange.end}</h3>
       </div>
