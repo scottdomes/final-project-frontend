0c770c1ee451e261e47db955961202d6f5fc8643
scottdomes <scottdomes@gmail.com>
2016-04-21 13:56:45 -0700

    Got browser history working, but reverted back to hash history for development

--- app/config/routes.jsx
+++ app/config/routes.jsx
@@ -3,6 +3,7 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
+var browserHistory = ReactRouter.browserHistory;
 var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main.jsx');
 var HelloWorld = require('../components/HelloWorld.jsx');
