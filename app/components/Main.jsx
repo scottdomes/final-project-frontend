8b5a24128df33c844a67b1012b80ec12ee272d70
scottdomes <scottdomes@gmail.com>
2016-04-20 17:30:29 -0700

    Facebook detail now lives in Main

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,11 +1,77 @@
 var React = require('react');
+var Facebook = require('../components/Facebook.jsx');
+var $ = require('jquery');
 
 var Main = React.createClass({
   getInitialState: function () {
     return { 
+      loggedin: false,
+      user_name: '',
       locationInput: '',
       dateRange: {}
     }
+  },
+  initializeFB_SDK: function () {
+    var thisComponent = this;
+    window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '238732356487269',
+          cookie     : true, 
+          xfbml      : true,
+          version    : 'v2.6'
+        });
+
+      FB.getLoginStatus(function(response) {
+          thisComponent.statusChangeCallback(response);
+      });
+    }
+  },
+  statusChangeCallback: function (response)  {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      this.testAPI();
+      this.setState({
+        loggedin: true
+      });
+      console.log("Logged in is " + this.state.loggedin);
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+    }
+  },
+  testAPI: function () {
+    var thisComponent = this;
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me?fields=email,name,gender', function(response) {
+      console.log('Successful login for: ' + response.name);
+      console.log(response.id);
+      $.ajax({
+        url: "http://localhost:3000/api/users",
+        type: "POST",
+        data: { fb_id: response.id },
+        success: function (res) {
+          thisComponent.setName(response.name);
+        }
+      });
+    });
+  },
+  setName: function (name) {
+    this.setState({
+      user_name: name
+    });
+  },
+  componentDidMount: function () {
+    Facebook.load(document, 'script', 'facebook-jssdk');
+    this.initializeFB_SDK();
   },
   handleNewInput: function (newInput) {
     this.setState({
@@ -29,7 +95,9 @@ var Main = React.createClass({
               onNewInput: this.handleNewInput,
               onNewDate: this.handleNewDate,
               locationInput: this.state.locationInput,
-              dateRange: this.state.dateRange
+              dateRange: this.state.dateRange,
+              loggedin: this.state.loggedin,
+              userName: this.state.user_name
             }
         );
     return (
--- app/containers/LandingContainer.jsx
+++ app/containers/LandingContainer.jsx
@@ -1,5 +1,4 @@
 var React = require('react');
-var $ = require('jquery');
 var LandingHeader = require('../components/LandingPage/LandingHeader.jsx');
 var LandingForm = require('../components/LandingPage/LandingForm.jsx');
 var LandingFBLogin = require('../components/LandingPage/LandingFBLogin.jsx');
@@ -12,72 +11,8 @@ var LandingContainer = React.createClass({
   },
   getInitialState: function () {
     return {
-      input: '',
-      loggedin: false,
-      user_name: ''
+      input: ''
     }
-  },
-  initializeFB_SDK: function () {
-    var thisComponent = this;
-    window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '238732356487269',
-          cookie     : true, 
-          xfbml      : true,
-          version    : 'v2.6'
-        });
-
-      FB.getLoginStatus(function(response) {
-          thisComponent.statusChangeCallback(response);
-      });
-    }
-  },
-  statusChangeCallback: function (response)  {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      this.testAPI();
-      this.setState({
-        loggedin: true
-      });
-      console.log("Logged in is " + this.state.loggedin);
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-    }
-  },
-  testAPI: function () {
-    var thisComponent = this;
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me?fields=email,name,gender', function(response) {
-      console.log('Successful login for: ' + response.name);
-      console.log(response.id);
-      $.ajax({
-        url: "http://localhost:3000/api/users",
-        type: "POST",
-        data: { fb_id: response.id },
-        success: function (res) {
-          thisComponent.setName(response.name);
-        }
-      });
-    });
-  },
-  setName: function (name) {
-    this.setState({
-      user_name: name
-    });
-  },
-  componentDidMount: function () {
-    Facebook.load(document, 'script', 'facebook-jssdk');
-    this.initializeFB_SDK();
   },
   handleLogin: function () {
     var thisComponent = this;
@@ -105,11 +40,11 @@ var LandingContainer = React.createClass({
     return (
       <div>
         <LandingHeader />
-        { this.state.loggedin ? 
+        { this.props.loggedin ? 
           <LandingForm 
             onNewInput={this.handleNewInput} 
             onSubmit={this.handleSubmit} 
-            userName={this.state.user_name}
+            userName={this.props.userName}
           />
           : <LandingFBLogin onLogin={this.handleLogin} />
         }
