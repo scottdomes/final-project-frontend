78f5c27558f7699570fdb854ba81b85895f82c5e
scottdomes <scottdomes@gmail.com>
2016-04-23 11:03:42 -0700

    Fixed bug where start and end dates were always set to the 2nd

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -55,8 +55,8 @@ var Main = React.createClass({
   handleNewDate: function(range) {
     this.setState({
       dateRange: {
-        start_date: range.start.format('MMMM do YYYY'),
-        end_date: range.end.format('MMMM do YYYY')
+        start_date: range.start.format('MMMM Do YYYY'),
+        end_date: range.end.format('MMMM Do YYYY')
       }
     });
   },
