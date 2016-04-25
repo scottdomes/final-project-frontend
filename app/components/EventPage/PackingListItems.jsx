215ee78fd1d441387a338cf3df41eec2f2cc8869
Filip Juristovski <fjuristovski@gmail.com>
2016-04-24 18:42:19 -0700

    cleaned up packing list item to properly show who is packing it with image and name

--- app/components/EventPage/PackingListItems.jsx
+++ app/components/EventPage/PackingListItems.jsx
@@ -12,11 +12,19 @@ function PackingListItem(props) {
     // props.onClick(e, props.item.label, props.onClick.__reactBoundArguments[0]);
     props.onUserPacksItem(e, props.item.label);
   }
-  
+  console.log(props.packer)
   return(
-    <div 
-      className={classNames({ 'item-packed': props.item.user_id ? true: false }, {"list-item": true})} 
-      onClick={handleUserPacksItem}>{'+ ' + props.item.label}{props.packer && <p>{props.packer[0].name}</p>}
+    <div>
+      <div 
+        className={classNames({ 'item-packed': props.item.user_id ? true: false }, {"list-item": true})} 
+        onClick={handleUserPacksItem}>
+        <div className="packer-left-div">
+        {'+ ' + props.item.label}
+        {props.packer && <span className="packer-name">{props.packer[0].name}</span>}
+        </div>
+        {props.packer && <img className='packer-image' src={props.packer[0].picture_path} />}
+
+      </div>
     </div>
   )
 
