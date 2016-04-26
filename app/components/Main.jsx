b52ac7ab293d99bb616115b3648894eb7be140b4
Filip Juristovski <fjuristovski@gmail.com>
2016-04-25 17:00:11 -0700

    fixed bug with asynchronous calling of events and packing list, implemented promises in load event

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -181,13 +181,11 @@ var Main = React.createClass({
     })
   },
   loadEvent: function (event_id) {
+
     var eventID = event_id ? event_id : this.props.params.id;
     var path = 'http://localhost:3000/api/events/' + eventID;
-    $.getJSON(path, function (data) {
-      console.log('load event called');
-      console.log(data);
-      console.log('event id is')
-      console.log(event_id);
+    $.getJSON(path)
+    .then(function (data) {
       this.setState({
         currentEventDetails: data.details,
         currentEventCreator: data.creator,
@@ -207,18 +205,73 @@ var Main = React.createClass({
       });
       this.isUserCreator();
       this.setFinalLocationAndDate(this.state.final_date_id, this.state.final_location_id)
-    }.bind(this));
 
-    $.getJSON('http://localhost:3000/api/items/' + event_id, function (data) {
-      console.log('in api item call')
-      console.log('event id is')
-      console.log(event_id);
-      console.log('data is ')
-      console.log(data)
+      return $.getJSON('http://localhost:3000/api/items/' + this.state.event_id)
+
+    }.bind(this))
+    .then(function (data) {
       this.setState({
         packingList: data.items,
       });
     }.bind(this));
+    
+    ////!!!!
+    ///OLD LOAD EVENT BELOW
+    //OLD IMPLEMENTATION
+    // var eventID = event_id ? event_id : this.props.params.id;
+    // var path = 'http://localhost:3000/api/events/' + eventID;
+    // $.getJSON(path, function (data) {
+    //   console.log('load event called');
+    //   console.log(data);
+    //   console.log('event id is')
+    //   console.log(event_id);
+    //   this.setState({
+    //     currentEventDetails: data.details,
+    //     currentEventCreator: data.creator,
+    //     eventName: data.details.name,
+    //     event_id: data.details.id,
+    //     dateRanges: data.dates,
+    //     locations: data.campsites,
+    //     eventParticipants: data.users,
+    //     eventCreatorID: data.details.user_id,
+    //     vote_on_location: data.details.vote_on_location,
+    //     vote_on_date: data.details.vote_on_date,
+    //     final_location_id: data.details.final_location_id,
+    //     final_date_id: data.details.final_date_id,
+    //     currentUserVotedDate: this.checkIfVoted(data.dates, "date"),
+    //     currentUserVotedLocation: this.checkIfVoted(data.campsites, "campsite"),
+    //     currentUserAddedDate: this.checkIfAddedDate(data.dates)
+    //   });
+    //   this.isUserCreator();
+    //   this.setFinalLocationAndDate(this.state.final_date_id, this.state.final_location_id)
+    
+    //   $.getJSON('http://localhost:3000/api/items/' + this.state.event_id, function (data) {
+    //     console.log('in api item call')
+    //     console.log('event id is')
+    //     console.log(this.state.event_id);
+    //     console.log('data is ')
+    //     console.log(data)
+    //     this.setState({
+    //       packingList: data.items,
+    //     });
+    //   }.bind(this));
+    // }.bind(this));
+    ////!!!
+    //OLD LOAD EVENT ABOVE
+
+
+    ///THIS IS STUFF DOESN"T MATTER HERE YOU CAN IGNORE FOR NOW
+    // put this inside ajax call or else!
+    // $.getJSON('http://localhost:3000/api/items/' + this.state.event_id, function (data) {
+    //   console.log('in api item call')
+    //   console.log('event id is')
+    //   console.log(this.state.event_id);
+    //   console.log('data is ')
+    //   console.log(data)
+    //   this.setState({
+    //     packingList: data.items,
+    //   });
+    // }.bind(this));
 
     //KEEP THIS HERE, MAY NEED TO REIMPLEMENT
     // $.getJSON('http://localhost:3000/api/items', function (data) {
