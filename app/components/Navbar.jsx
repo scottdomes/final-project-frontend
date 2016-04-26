f67c7a2792aa26f984a22250c65fe8c142d8f6b1
scottdomes <scottdomes@gmail.com>
2016-04-25 19:08:07 -0700

    Fixed sidebar styles so that events reappear

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -17,17 +17,12 @@ var Navbar = React.createClass({
           { this.props.loggedin ?
           <div className="loggedin-container"><p id="loggedin-indicator">Welcome back, <span>{this.props.userName}</span></p>
           <button className="button tiny" onClick={this.handleLogout}>Logout</button></div> : <span></span> }
-          <div className="events-sidebar">
-            <span>Events Created</span>
-            {this.props.eventsCreated}
-            <span>Events Attending</span>
-            {this.props.eventsAttended}
-          </div>
-          <div className="column large-6 large-offset-2 text-right">
-            { this.props.loggedin ?
-            <div className="loggedin-container"><p id="loggedin-indicator">Welcome back, <span>{this.props.userName}</span></p>
-            <button className="button tiny secondary" onClick={this.handleLogout}>Logout</button></div> : <span></span> }
-          </div> 
+        </div>
+        <div className="events-sidebar">
+          <span>Events Created</span>
+          {this.props.eventsCreated}
+          <span>Events Attending</span>
+          {this.props.eventsAttended}
         </div>
       </div>
     )
