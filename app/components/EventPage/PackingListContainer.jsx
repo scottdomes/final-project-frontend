1b5e742c39a2cae38e7b0dae0fab4586c01bd500
Filip Juristovski <fjuristovski@gmail.com>
2016-04-26 10:33:04 -0700

    refactored code of packinglistcontainer

--- app/components/EventPage/PackingListContainer.jsx
+++ app/components/EventPage/PackingListContainer.jsx
@@ -22,34 +22,66 @@ var PackingListContainer = React.createClass({
   handleEnterNewItem: function (value){
     this.props.onEnterNewItem(value);
   },
+  getPackingListType: function (listType){
+
+
+    if (listType === 'public'){
+      if (this.props.packingList){
+        var ItemList = this.props.packingList;
+        //creates an array of components to display
+        var PackingItems = ItemList.map((item, index) => {
+        
+        //if the item has a user id, goes through user list and gets the packer which is a user item (public only)
+        if (item.user_id){
+          var packer = this.props.userList.filter(function(user){
+            return item.user_id == user.id ? true : false;
+          });
+        }
+
+        return <PackingListItem packer={packer} onUserPacksItem={this.handleUserPacksItem.bind(this, index)} key={index} item={item} />
+      });
+      }
+    }
+
+    return PackingItems;
+  
+  },
   render: function () {
     const {newPackingItem} = this.state
-    var PackingList = [];
-    if (this.props.packingList){
-      var ItemList = this.props.packingList;
-      var PackingItems = ItemList.map((item, index) => {
+    // var PackingList = [];
+    // if (this.props.packingList){
+    //   var ItemList = this.props.packingList;
+    //   var PackingItems = ItemList.map((item, index) => {
       
-      if (item.user_id){
-        var packer = this.props.userList.filter(function(user){
-          return item.user_id == user.id ? true : false;
-        });
-      }
-      return <PackingListItem packer={packer} onUserPacksItem={this.handleUserPacksItem.bind(this, index)} key={index} item={item} />
-    });
-    }
-  
+    //   if (item.user_id){
+    //     var packer = this.props.userList.filter(function(user){
+    //       return item.user_id == user.id ? true : false;
+    //     });
+    //   }
+    //   return <PackingListItem packer={packer} onUserPacksItem={this.handleUserPacksItem.bind(this, index)} key={index} item={item} />
+    // });
+    // }
+
+    PackingItems = this.getPackingListType('public');
 
     return (
       <div id="packing-list-container">
         <div>
           <h2 id="packing-list-header">Packing List</h2>
         </div>
-        {PackingItems}
+        <div id="public-packing-list">
+          <h5>My Packing List</h5>
+        </div>
+        <div>
+          {PackingItems}
+        </div>
         <AddMorePackingItemsForm 
           itemDescription={newPackingItem} 
           onChange={this.handleNewPackingItemChange}
           onKeyDown={this.handleEnterNewItem}/>
       </div>
+
+
     
     )
   }
