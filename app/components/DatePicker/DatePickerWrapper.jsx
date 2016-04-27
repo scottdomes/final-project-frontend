270d17ea497a21cb3f362c72716c6ccdd83767a4
Alvin Chen <alvinchen1988@icloud.com>
2016-04-27 16:01:43 -0700

    fix calendar

--- app/components/DatePicker/DatePickerWrapper.jsx
+++ app/components/DatePicker/DatePickerWrapper.jsx
@@ -4,7 +4,7 @@ var DatePickerWrapper = React.createClass({
   render: function () {
     return (
       <div className="row" id="enter-location" style={{marginTop: "35px"}}>
-        <div className="large-6 large-centered columns">
+        <div className="large-6 large-centered medium-8 medium-centered columns">
           <h4>When do you want to take this trip with friends?</h4>
           {this.props.children}
         </div>
