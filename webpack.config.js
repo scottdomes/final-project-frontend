9c8e1548fe848aa6364f5d50f380ec4643fe5165
Filip Juristovski <fjuristovski@gmail.com>
2016-06-15 20:20:57 -0700

    added babel npms and added .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   module: {
     loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader", query: { presets: ['es2015', 'react'] }},
       { test: /\.css$/, loader: 'style!css' },
       { test: /\.jsx$/, loaders: ['jsx-loader', 'react-hot'] }
     ]
