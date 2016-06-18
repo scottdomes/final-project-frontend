2381c2cc1b7b3e8df4ac691ddae573ef02902584
Filip Juristovski <fjuristovski@gmail.com>
2016-06-18 12:59:35 -0700

    refactored packinglistcontainer to use es6 classes

--- app/components/EventPage/PackingList/AddMorePackingItemsForm.js
+++ app/components/EventPage/PackingList/AddMorePackingItemsForm.js
@@ -71,37 +71,5 @@ export default class AddMorePackingItemsForm extends Component {
 
 }
 
-// var AddMorePackingItemsForm = React.createClass({
-//   handleChange (event){
-//     this.props.onChange(event.target.value, this.props.packingType);
-//   },
-//   handleEnterNewItem (event) {
-//     if( event.keyCode == 13 ){ //Enter key clicked
-//       this.props.onKeyDown(event.target.value, this.props.packingType);  
-//       event.target.value = ''
-//       event.target.placeholder = 'Add More'
-//     }
-//   },
-//   handleFormBlur: function (event) {
-//     this.props.onBlur(event.target.value, this.props.packingType);
-//   },
-//   render: function(){
-//     return (
-//         <input 
-//         id='packing-list-add-more-form' 
-//         type="text" 
-//         name="text" 
-//         onBlur={this.handleFormBlur}
-//         placeholder={this.props.itemDescription} 
-//         onChange={this.handleChange}
-//         onKeyDown={this.handleEnterNewItem}/>
-//       )
-//   }
-
-// });
-
-// module.exports = AddMorePackingItemsForm;
-
-
 
  
--- app/components/EventPage/PackingList/PackingListContainer.js
+++ app/components/EventPage/PackingList/PackingListContainer.js
@@ -1,20 +1,138 @@
-var React = require('react');
-// var PackingListItem = require('./PackingListItems');
-var classNames = require('classnames');
-// var AddMorePackingItemsForm = require('./AddMorePackingItemsForm');
+// var React = require('react');
+// // var PackingListItem = require('./PackingListItems');
+// var classNames = require('classnames');
+// // var AddMorePackingItemsForm = require('./AddMorePackingItemsForm');
 
-import PackingListTitle from './PackingListTitle'; //not sure if this is right import
+// import PackingListTitle from './PackingListTitle'; //not sure if this is right import
+// import PackingListItem from './PackingListItems';
+// import AddMorePackingItemsForm from './AddMorePackingItemsForm';
+
+// var PackingListContainer = React.createClass({
+//   getInitialState: function() {
+//       return {
+//         newPublicPackingItem: 'Add More',
+//         newPrivatePackingItem: 'Add More'
+//       }
+//   },
+//   handleUserPacksItem: function (key, e, itemLabel, listType){
+//     e.preventDefault();
+//     e.stopPropagation();
+//     if (listType === 'public') {
+//       var currentItem = this.props.packingList.publicPackingList[key];
+//     } else {
+//       var currentItem = this.props.packingList.privatePackingList[key];
+//     }
+//     this.props.onUserPacksItem(currentItem, key);
+//   },
+//   handleNewPackingItemChange: function (value, listType){
+//     if (listType === 'public'){
+//       this.setState({
+//         newPublicPackingItem: value
+//       });
+//     } else if (listType === 'private') {
+//       this.setState({
+//         newPrivatePackingItem: value
+//       });
+//     }
+//   },
+//   handleEnterNewItem: function (value, listType){
+//     this.props.onEnterNewItem(value, listType);
+//   },
+//   getPackingListItems: function (packingList, listType){
+
+//     if (packingList){ 
+//       var PackingItems = packingList.map((item, index) => {
+      
+//         var packer = null;
+//         if (listType === 'public'){
+//           if (item.user_id){
+//             packer = this.props.userList.filter(function(user){
+//               return item.user_id == user.id ? true : false;
+//             });
+//           }
+//         }
+//         return <PackingListItem packer={packer} listType={listType} onUserPacksItem={this.handleUserPacksItem.bind(this, index)} key={item.id} item={item} />
+//       });
+//     }
+
+//     return PackingItems;
+//   },
+//   handleFormBlur: function (value, listType){
+//     if (listType === 'public' && value === ''){
+//       this.setState({
+//         newPublicPackingItem: 'Add More'
+//       });
+//     } else if (listType === 'private'  && value === '') {
+//       this.setState({
+//         newPrivatePackingItem: 'Add More'
+//       });
+//     }
+//   },
+//   render: function () {
+//     var {newPublicPackingItem, newPrivatePackingItem} = this.state
+
+//     var publicPackingItems = this.getPackingListItems(this.props.packingList.publicPackingList, 'public');
+//     var privatePackingItems = this.getPackingListItems(this.props.packingList.privatePackingList, 'private');
+
+//     return (
+//       <div id="packing-list-container">
+        
+//         <div>
+//           <PackingListTitle title="Group Packing List"/>
+
+//           <div className="packing-list-section-heading">
+//             <h5>Group Packing List</h5>
+//           </div>
+//           <div>
+//             {publicPackingItems}
+//           </div>
+//           <AddMorePackingItemsForm 
+//             packingType='public'
+//             onBlur={this.handleFormBlur}
+//             itemDescription={newPublicPackingItem} 
+//             onChange={this.handleNewPackingItemChange}
+//             onKeyDown={this.handleEnterNewItem}/>
+//         </div>
+//         <div>
+//           <div className="packing-list-section-heading">
+//             <h5>My Packing List</h5>
+//           </div>
+//           <div>
+//             {privatePackingItems}
+//           </div>
+//           <AddMorePackingItemsForm 
+//             packingType='private'
+//             onBlur={this.handleFormBlur}
+//             itemDescription={newPrivatePackingItem} 
+//             onChange={this.handleNewPackingItemChange}
+//             onKeyDown={this.handleEnterNewItem}/>
+//         </div>
+//       </div>
+//     )
+//   }
+// });
+
+// module.exports = PackingListContainer;
+
+import React, {Component, PropTypes} from 'react'; //import library and components of library
+import classNames from 'classnames';
+import PackingListTitle from './PackingListTitle';
 import PackingListItem from './PackingListItems';
 import AddMorePackingItemsForm from './AddMorePackingItemsForm';
 
-var PackingListContainer = React.createClass({
-  getInitialState: function() {
-      return {
-        newPublicPackingItem: 'Add More',
-        newPrivatePackingItem: 'Add More'
-      }
-  },
-  handleUserPacksItem: function (key, e, itemLabel, listType){
+export default class PackingListContainer extends Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      newPublicPackingItem: 'Add More',
+      newPrivatePackingItem: 'Add More'
+    }
+
+  }
+
+ handleUserPacksItem = (key, e, itemLabel, listType) => {
     e.preventDefault();
     e.stopPropagation();
     if (listType === 'public') {
@@ -23,8 +141,9 @@ var PackingListContainer = React.createClass({
       var currentItem = this.props.packingList.privatePackingList[key];
     }
     this.props.onUserPacksItem(currentItem, key);
-  },
-  handleNewPackingItemChange: function (value, listType){
+  }
+
+  handleNewPackingItemChange = (value, listType) => {
     if (listType === 'public'){
       this.setState({
         newPublicPackingItem: value
@@ -34,11 +153,13 @@ var PackingListContainer = React.createClass({
         newPrivatePackingItem: value
       });
     }
-  },
-  handleEnterNewItem: function (value, listType){
+  }
+
+  handleEnterNewItem = (value, listType) => {
     this.props.onEnterNewItem(value, listType);
-  },
-  getPackingListItems: function (packingList, listType){
+  }
+
+  getPackingListItems = (packingList, listType) => {
 
     if (packingList){ 
       var PackingItems = packingList.map((item, index) => {
@@ -56,8 +177,9 @@ var PackingListContainer = React.createClass({
     }
 
     return PackingItems;
-  },
-  handleFormBlur: function (value, listType){
+  }
+
+  handleFormBlur = (value, listType) => {
     if (listType === 'public' && value === ''){
       this.setState({
         newPublicPackingItem: 'Add More'
@@ -67,8 +189,9 @@ var PackingListContainer = React.createClass({
         newPrivatePackingItem: 'Add More'
       });
     }
-  },
-  render: function () {
+  }
+
+  render () {
     var {newPublicPackingItem, newPrivatePackingItem} = this.state
 
     var publicPackingItems = this.getPackingListItems(this.props.packingList.publicPackingList, 'public');
@@ -110,11 +233,10 @@ var PackingListContainer = React.createClass({
       </div>
     )
   }
-});
+
+}
 
 module.exports = PackingListContainer;
-
-
 
 
 
